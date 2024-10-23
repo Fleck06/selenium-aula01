ae998a9307946482223278160d7ac5c58eabe4b8
Mateus Fleck <mateus.fleck@duque.g12.br>
2024-10-22 21:46:07 -0300

    First Commit

--- trabalho.py
+++ trabalho.py
@@ -2,3 +2,11 @@ from selenium import webdriver
 from time import sleep
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
+
+opts = ChromeOptions()
+opts.add_experimental_option("detach", True)
+
+# Passe a variavel opts para o webdriver
+navegador = webdriver.Chrome(options = opts)
+
+url1 = r'https://magazineluiza.com.br/'
